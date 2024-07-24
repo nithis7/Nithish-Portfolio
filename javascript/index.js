59b9cc235f5587e0e893590839c83a635b919763
nithis7 <nithishkumar200605@gmail.com>
2024-07-24 16:15:45 +0530

    Edit

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+/* switch case
+let car="Benz";
+switch(car){
+    case"Benz":
+        console.log("Benz is selected")
+        break;
+    case"BMW":
+        console.log("BMW is selected")
+        break;
+}
+
+for loop
+for(let i=0;i<5;i++){
+    console.log(i)
+}
+
+let person={
+    name: "Nithish Kumar R"
+}
+for(let key in person){
+    console.log(key+" : "+person.key)
+} */
